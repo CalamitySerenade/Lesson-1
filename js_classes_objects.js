3400bbd7cff6e332df9b5bcbe15cad2ec9d8cba0
CalamoitySerenade <wilsongarate1@gmail.com>
2025-10-15 09:18:41 -0400

    Classwork for 10/15

---
+++ js_classes_objects.js
@@ -0,0 +1,20 @@
+//What is the basic syntax for writing a JS class?
+
+class ClassName{
+    constructor(name, year){
+        this.name=name;
+        this.year=year;
+    }
+}
+
+
+//What is the basic syntax for writing a JS object
+const student1 = new ClassName("Example1",2026);
+const student2 = {
+    name:"Example2",
+    year:2021
+};
+const student3 = new Object({
+    name: "Example3",
+    year:1991
+});
