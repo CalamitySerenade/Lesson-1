99592919e17f7a3742e5e0c0b39c4afff8e64202
CalamoitySerenade <wilsongarate1@gmail.com>
2025-10-28 08:54:08 -0400

    d

--- js_loops_arrays.js
+++ js_loops_arrays.js
@@ -1,1 +1,16 @@
 //Today's objective is to go over JS for loops, arrays, and for each
+let array=[1,2,3,4,5,6];
+//length=6, index=5
+console.log(array);
+
+//Basic notes:
+//1. Array index always starts at 0
+//2. FormulaIndex= Array.length-1
+
+//Adds 2 to the original values
+for(let i=0;i<array.length;i++){
+    console.log(array[i]+2);
+}
+
+let array2=[1,2,3];
+for
